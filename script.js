b5c97a802095dbe56f9c8c4378d108604f4dcc88
Elijah Emmaneuel Hienwo <emmanuelhienwo@gmail.com>
2025-12-15 12:02:45 +0000

    Initial Team Site update

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("welcomeBtn");
+const message = document.getElementById("message");
+
+button.addEventListener("click", () => {
+    message.textContent = "Welcome! Collaboration starts here 🚀";
+});
